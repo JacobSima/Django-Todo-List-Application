bfd7f074fd3599f41158dcc305f4127517c1cc42
Jacob Sima <simajacob2011@gmail.com>
2020-04-09 19:42:59 +0200

    complete a basic CRUD with Django

--- main/todos/urls.py
+++ main/todos/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home,add_todo,delete_todo
+from .views import home,add_todo,delete_todo,edit_todo
 
 app_name = 'todos'
 
 urlpatterns = [
   path('',home,name='todos-list'),
   path('add/',add_todo,name='todos-add'),
-  path('<int:id>/delete',delete_todo, name='todos-delete')
+  path('<int:id>/delete/',delete_todo, name='todos-delete'),
+  path('<int:id>/edit/',edit_todo, name='todos-edit'),
 ]
--- main/todos/views.py
+++ main/todos/views.py
@@ -1,7 +1,9 @@
+from __future__ import unicode_literals
 from django.shortcuts import render,HttpResponse,HttpResponseRedirect,redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
 from .models import Todo
+
 
 # Create your views here.
 def mainHome(request):
@@ -18,7 +20,8 @@ def home(request):
 
 @csrf_exempt
 def add_todo(request):
-  added_date = timezone.now()
+  date = timezone.now()
+  added_date = date.strftime('%Y-%m-%d')
   text = request.POST.get('text')
   obj = Todo.objects.create(added_date=added_date,text=text)
   return redirect('/todos/1/')
@@ -28,5 +31,21 @@ def delete_todo(request,id):
   obj = Todo.objects.get(id = id)
   obj.delete()
   return redirect('/todos/1/')
+
+@csrf_exempt
+def edit_todo(request,id):
+  obj = Todo.objects.get(id = id)
+  if request.method == 'GET':
+    context ={'obj':obj}
+    return render(request,'todo-edit.html',context)
+  elif request.method == 'POST':
+    added_date = request.POST.get('date')
+    text = request.POST.get('text')
+    Todo.objects.filter(id = id).update(
+      added_date = added_date,
+      text = text
+    )
+  return redirect('/todos/1/')
+
 
 
